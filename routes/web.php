cab7c54789a45d5573f4a4a89d19352299226615
dAsHMeLnyK <daryna.melnyk@oa.edu.ua>
2024-06-19 17:14:25 +0300

    13

--- routes/web.php
+++ routes/web.php
@@ -8,43 +8,38 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
-
 Route::resource('rest', RestTestController::class)->names('restTest');
-Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
+
+Route::group(['namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', PostController::class)->names('blog.posts');
 });
 
-
-//Адмінка
+// Адмінка
 $groupData = [
     'namespace' => 'App\Http\Controllers\Blog\Admin',
     'prefix' => 'admin/blog',
 ];
 Route::group($groupData, function () {
-    //BlogCategory
-    $methods = ['index','edit','store','update','create',];
+    // BlogCategory
+    $methods = ['index', 'edit', 'store', 'update', 'create'];
     Route::resource('categories', CategoryController::class)
-    ->only($methods)
-    ->names('blog.admin.categories'); 
-    //BlogPost
-    Route::resource('posts', PostController::class)
-    ->except(['show'])                               //не робити маршрут для метода show
-    ->names('blog.admin.posts');
+        ->only($methods)
+        ->names('blog.admin.categories'); 
     
- });
- Route::group(['prefix' => 'digging_deeper'], function () {
-
+     //BlogPost
+     Route::resource('posts', PostController::class)
+     ->except(['show'])                               //не робити маршрут для метода show
+     ->names('blog.admin.posts');
+});
+Route::group(['prefix' => 'digging_deeper'], function () {
     Route::get('collections', [DiggingDeeperController::class, 'collections'])
-
         ->name('digging_deeper.collections');
+
+    Route::get('process-video', [DiggingDeeperController::class, 'processVideo'])
+    ->name('digging_deeper.processVideo');
+    
+    Route::get('prepare-catalog', [DiggingDeeperController::class, 'prepareCatalog'])
+    ->name('digging_deeper.prepareCatalog'); 
+
 
 });
